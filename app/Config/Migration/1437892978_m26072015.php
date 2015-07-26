dc449e7daa0de2c2046147fca8f2c420221360b5
Lucas Nadalutti <lucasnadalutti@gmail.com>
2015-07-26 03:43:56 -0300

    Testing migrations

--- app/Config/Migration/1437892978_m26072015.php
+++ app/Config/Migration/1437892978_m26072015.php
@@ -18,6 +18,7 @@ class M26072015 extends CakeMigration {
 			'create_table' => array(
 				'users' => array(
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 					'indexes' => array(
 						'PRIMARY' => array('column' => 'id', 'unique' => 1),
 					),
