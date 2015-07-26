68638bce978abef9f38b4171e843a5195309343e
Lucas Nadalutti <lucasnadalutti@gmail.com>
2015-07-26 01:38:01 -0300

    Testing migrations

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -8,4 +8,12 @@ class AppSchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
+	public $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
 }
