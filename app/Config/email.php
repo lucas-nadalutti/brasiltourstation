5e01978c58bcde7ce3083e88c760ff73b4bef9ca
Lucas Nadalutti <lucasnadalutti@gmail.com>
2015-07-26 12:46:36 -0300

    Email configurations

--- app/Config/email.php
+++ app/Config/email.php
@@ -39,56 +39,22 @@
  */
 class EmailConfig {
 
+	/***
+	
+	----------------------------- TODO REMINDER --------------------------------
+
+	MUST CHANGE THESE VALUES IN STAGING AND PRODUCTION ENVIRONMENTS WHEN THERE
+	IS AN OFFICIAL EMAIL
+
+	***/
+
 	public $default = array(
-		'transport' => 'Mail',
-		'from' => 'you@localhost',
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
-	public $smtp = array(
-		'transport' => 'Smtp',
-		'from' => array('site@localhost' => 'My Site'),
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => false,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
-	public $fast = array(
-		'from' => 'you@localhost',
-		'sender' => null,
-		'to' => null,
-		'cc' => null,
-		'bcc' => null,
-		'replyTo' => null,
-		'readReceipt' => null,
-		'returnPath' => null,
-		'messageId' => true,
-		'subject' => null,
-		'message' => null,
-		'headers' => null,
-		'viewRender' => null,
-		'template' => false,
-		'layout' => false,
-		'viewVars' => null,
-		'attachments' => null,
-		'emailFormat' => null,
-		'transport' => 'Smtp',
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => true,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
+		'transport' => getenv('EMAIL_TRANSPORT'),
+	    'host' => getenv('EMAIL_HOST'),
+	    'port' => getenv('EMAIL_PORT'),
+	    'username' => getenv('EMAIL_USERNAME'),
+	    'password' => getenv('EMAIL_PASSWORD'),
+	    'tls' => true,
 	);
 
 }
