99dcd08834a67f0871231af66bdde67f041f3105
Lucas Nadalutti <lucasnadalutti@gmail.com>
2015-07-26 02:35:11 -0300

    Testing migrations

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -4,4 +4,9 @@ App::uses('AppController', 'Controller');
 
 class UsersController extends AppController {
 
+	public function index() {
+		$user = array('User' => array('name' => 'bla'));
+		$this->User->save($user);
+	}
+
 }
