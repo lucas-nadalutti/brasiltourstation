a5674b47f285381203cb011336d2cdf8fa837890
Lucas Nadalutti <lucasnadalutti@gmail.com>
2015-07-26 14:28:15 -0300

    Testing production deploy

--- app/Config/database.php
+++ app/Config/database.php
@@ -88,6 +88,11 @@ if (!defined('RDS_HOSTNAME')) {
 	define('TEST_DB_NAME', getenv('TEST_DB_NAME'));
 }
 
+print_r(RDS_HOSTNAME);
+print_r(RDS_USERNAME);
+print_r(RDS_PASSWORD);
+print_r(RDS_DB_NAME);
+
 class DATABASE_CONFIG {
 
 	public function __construct() {
